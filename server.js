aa456f9aca0bb7f7d23e1148a6f6f6f9317fb62d
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-11-22 18:57:05 +0530

    url added

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://sujana-gold.vercel.app/'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
